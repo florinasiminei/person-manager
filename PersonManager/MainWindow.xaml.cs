55471f2e915d2d42f02d523886c208883a1675f6
Florin <asiminei_florin@yahoo.com>
2019-09-24 23:08:45 +0300

    First Commit; Add to listView button

---
+++ PersonManager/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using PersonManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersonManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //get the listView object
+            ListView availablePersonsLV = this.FindName("availablePersonsLV") as ListView;
+
+
+        }
+
+        private void Add_New_Person_Click(object sender, RoutedEventArgs e)
+        {
+            //create a person model using textbox input
+            PersonModel personModel = new PersonModel(first_name_txt.Text, last_name_txt.Text, "cartofi prajiti");
+            
+            //add person to the List View to be displayed
+            availablePersonsLV.Items.Add(personModel);
+
+            //clear the textbox
+            first_name_txt.Text = "";
+            last_name_txt.Text = "";
+
+            //print the added person to output for debugging
+            Console.WriteLine(personModel);
+        }
+
+
+    }
+}
---
+++ PersonManager/Models/PersonModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonManager.Models
+{
+    class PersonModel
+    {
+        private string _first_name;
+        private string _last_name;
+        private string _menu;
+
+        public PersonModel(string first_name, string last_name, string menu)
+        {
+            _first_name = first_name;
+            _last_name = last_name;
+            _menu = menu;
+        }
+
+        public string FirstName
+        {
+            get { return _first_name; }
+            set { _first_name = value; }
+        }
+        public string LastName
+        {
+            get { return _last_name; }
+            set { _last_name = value; }
+        }
+        public string Menu
+        {
+            get { return _menu; }
+            set { _menu = value; }
+        }
+
+        //this method is used to print nicely a Person object in the ListView
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", FirstName, LastName);
+        }
+    }
+}
